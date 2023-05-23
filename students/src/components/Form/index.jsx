22b8549a1526215520926933a28597d5f8e76f4c
Salomão Santos <salomaosantos3949@gmail.com>
2023-05-22 22:25:03 -0300

    feat: estrturando o projeto

---
+++ students/src/components/Form/index.jsx
@@ -0,0 +1,14 @@
+
+export const index = () => {
+  return (
+    <div>
+      <input type="text"></input>
+      <input type="text"></input>
+      <select name="" id="">
+        <option value="">Curso</option>
+      </select>
+      <input type="text"></input>
+      <button>Salvar</button>
+    </div>
+  )
+}
